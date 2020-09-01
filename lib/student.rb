b669aa61f215ce71ab0219289cb5860b3b058979
Bria White <bria.awhite@gmail.com>
2020-09-01 23:10:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   
   def self.drop_table
     sql =  <<-SQL 
-      DROP TABLE students (
+      DROP TABLE IF EXIST students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade TEXT
