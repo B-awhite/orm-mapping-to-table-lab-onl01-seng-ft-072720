f78d9f1eb195f5e07a5b75cd383bac43bf942c8d
Bria White <bria.awhite@gmail.com>
2020-09-01 23:27:07 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 
+def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
