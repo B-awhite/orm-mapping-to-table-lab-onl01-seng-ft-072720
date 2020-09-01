600d7a6c27d76e7b7dc3e241259eb9ed03a3454c
Bria White <bria.awhite@gmail.com>
2020-09-01 22:51:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id 
   
   def initialize(name, grade, id=nil)
     @name = name
-    @grade = grade 
+    @grade = grade
+    @id = id
   end 
   
   
